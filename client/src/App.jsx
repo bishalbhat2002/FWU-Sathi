888b317de5b904991b7f83d7903c378da4fdae6f
Bishal Bhat <bishalbhat2002@gmail.com>
2026-01-26 11:15:19 +0545

    FrontEnd start

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-black text-8xl flex justify-center items-center ">
+        <h1 className="animate-bounce bg-linear-350 from-cyan-300 to-green-500 font-bold bg-clip-text text-transparent">
+        FWU Sathi....
+        </h1>
+    </div>
+  )
+}
+
+export default App
